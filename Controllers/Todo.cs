80e640982b078789260516ce1aa15624d5538b43
Iliyan <iliyan.yankov@channelcapital.io>
2022-11-18 17:43:19 +0000

    controller connected to the database

--- Controllers/Todo.cs
+++ Controllers/Todo.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetEntity.Data;
 using NetEntity.Models;
 
 namespace NetEntity.Controllers;
@@ -7,47 +9,55 @@ namespace NetEntity.Controllers;
 [ApiController]
 public class Todo : Controller
 {
-  private static List<Models.Todo> todoList = new();
+  private readonly DataContext _context;
+
+  public Todo(DataContext context)
+  {
+    _context = context;
+  }
 
   [HttpGet]
   public async Task<ActionResult<List<Models.Todo>>> Get()
   {
-    return Ok(todoList);
+    return Ok(await _context.Todos.ToListAsync());
   }
 
   [HttpPost]
   public async Task<ActionResult<CustomApiResponse>> AddTodo(Models.Todo todo)
   {
-    todoList.Add(todo);
+    _context.Todos.Add(todo);
+    await _context.SaveChangesAsync();
     return Ok(new CustomApiResponse("Success", todo));
   }
 
   [HttpPatch]
   public async Task<ActionResult<Models.Todo>> UpdateTodo(Models.Todo todo)
   {
-    var existingTodo = todoList.Find(t => t.Id == todo.Id);
+    var existingTodo = await _context.Todos.FindAsync(todo.Id);
     if (existingTodo == null) return BadRequest("Todo not found");
     existingTodo.Description = todo.Description;
     existingTodo.Done = todo.Done;
     existingTodo.Title = todo.Title;
-
+    await _context.SaveChangesAsync();
     return Ok(existingTodo);
   }
 
   [HttpGet("{id}")]
   public async Task<ActionResult<Models.Todo>> GetOne(int id)
   {
-    var existingTodo = todoList.Find(t => t.Id == id);
+    var existingTodo = await _context.Todos.FindAsync(id);
     if (existingTodo == null) return BadRequest("Todo not found");
     return Ok(existingTodo);
   }
 
 
-  [HttpDelete("{id}")]
+  [HttpDelete("{id:int}")]
   public async Task<ActionResult<CustomApiResponse>> DeleteOne(int id)
   {
-    var existingTodo = todoList.Find(t => t.Id == id);
+    var existingTodo = await _context.Todos.FindAsync(id);
     if (existingTodo == null) return BadRequest("Todo not found");
+    _context.Todos.Remove(existingTodo);
+    await _context.SaveChangesAsync();
     return Ok(new CustomApiResponse($"Todo {existingTodo.Title} deleted"));
   }
 }
--- readme.md
+++ readme.md
@@ -9,8 +9,16 @@
 ## Requirements
 
 - dotnet ef
+- docker mysql container
 
 ## Preparation
+
+- stat the database with docker `docker run --name my-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql`
+- create migration
+  `dotnet ef migrations add CreateInitial`
+- update the database
+  `dotnet ef database update`
+-
 
 ## How to run:
 
